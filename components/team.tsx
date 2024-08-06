819d361e39dc0b9189b0ad32fdf4cc237caa9da9
Parth Shandilya <24358501+ParthS007@users.noreply.github.com>
2024-08-06 09:13:00 +0530

    team: remove parth

--- components/team.tsx
+++ components/team.tsx
@@ -4,9 +4,7 @@ import Image from 'next/image'
 import TeamImage01 from '@/public/images/team-01-ab.jpeg'
 import TeamImage02 from '@/public/images/team-02-dl.jpeg'
 import TeamImage03 from '@/public/images/team-03-jl.jpeg'
-import TeamImage04 from '@/public/images/team-04-ps.jpeg'
 import TeamImage05 from '@/public/images/team-05-st.jpg'
-import TalentKick from '@/public/images/TalentKick.jpg'
 
 export default function Team() {
     return (
@@ -20,7 +18,7 @@ export default function Team() {
         </div>
 
         {/* Items */}
-        <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-4 lg:gap-10 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
+        <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-10 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
 
           {/* 1st item */}
           <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
@@ -46,13 +44,6 @@ export default function Team() {
             <p className="text-lg text-gray-400 text-center">Bachelor in Comp. Science ETH Zurich</p>
           </div>
 
-          {/* 4th item */}
-          <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="300" data-aos-anchor="[data-aos-id-blocks]">
-            <Image className="rounded-full" src={TeamImage04} width={128} height={128} alt="TeamImage 04" />
-            <h4 className="h4 mb-2">Parth Shandilya</h4>
-            <p className="text-lg text-gray-400 text-center">Co-Founder</p>
-            <p className="text-lg text-gray-400 text-center">Master in Comp. Science University of Basel</p>
-          </div>
         </div>
         <br></br>
         {/* Mentors and Advisors item */}
